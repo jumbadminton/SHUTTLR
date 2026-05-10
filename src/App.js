b40366a5190967fdfebadc2f2676bc918f31fd83
jumbadminton <aroeneyeldi@gmail.com>
2026-05-10 06:24:53 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -720,25 +720,3 @@ function Profile({ p, matches, onEdit, onDelete }) {
     </div>
   );
 }
-
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAVe8Uo-4dKA9NZSdmJFeebEnu5_HFGyaE",
-  authDomain: "shuttlr2026.firebaseapp.com",
-  projectId: "shuttlr2026",
-  storageBucket: "shuttlr2026.firebasestorage.app",
-  messagingSenderId: "1045654857416",
-  appId: "1:1045654857416:web:c6579942b55a94f67e27fd"
-};
-
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode><App /></React.StrictMode>);
